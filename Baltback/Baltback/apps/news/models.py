686c9b726b69bcfac11924375b2407016fee5208
DanilaEremenko <danilaeremenko@mail.ru>
2019-10-14 13:31:10 +0300

    -apps/news meta added, admin initialized

--- Baltback/Baltback/apps/news/models.py
+++ Baltback/Baltback/apps/news/models.py
@@ -1,10 +1,14 @@
 from django.db import models
 
 
-class News(models.Model):
-    title = models.CharField('title', max_length=150)
-    text = models.TextField('text')
-    pub_date = models.DateTimeField('date of publishing')
+class Article(models.Model):
+    title = models.CharField('Заголовок', max_length=150)
+    text = models.TextField('Текст статьи')
+    pub_date = models.DateTimeField('Дата публикации')
 
     def __str__(self):
         return self.title
+
+    class Meta:
+        verbose_name = 'Статья'
+        verbose_name_plural = 'Статьи'
--- Baltback/Baltback/apps/news/views.py
+++ Baltback/Baltback/apps/news/views.py
@@ -1,10 +1,10 @@
 from django.http import HttpResponse
-from .models import News
+from .models import Article
 
 
 def index(request):
     http_resp = ""
-    for news in News.objects.all():
+    for news in Article.objects.all():
         http_resp += "<h1>%s</h1>\n" % news.title
         http_resp += "<i>%s</i>\n" % news.text
         http_resp += "<br><b>%s</b>\n" % news.pub_date
