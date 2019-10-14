a8314e2c7a3f59aea0ad305f02d31931aad7df14
DanilaEremenko <danilaeremenko@mail.ru>
2019-10-14 13:31:42 +0300

    -apps/services meta added, admin initialized

--- Baltback/Baltback/apps/services/models.py
+++ Baltback/Baltback/apps/services/models.py
@@ -1,11 +1,15 @@
 from django.db import models
 
 
-# class Service(models.Model):
-#     title = models.CharField('title', max_length=50)
-#     intro = models.TextField('intro')
-#     json_path = models.CharField('json with list')
-#     outro = models.TextField('outro')
-#
-#     def __str__(self):
-#         return self.title
+class Service(models.Model):
+    title = models.CharField('Название услуги', max_length=50)
+    intro = models.TextField('Введение')
+    enum_json = models.TextField("Перечисление в формате json")
+    outro = models.TextField('Подытоживание')
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'Услуга'
+        verbose_name_plural = 'Услуги'
--- Baltback/Baltback/apps/services/views.py
+++ Baltback/Baltback/apps/services/views.py
@@ -1,18 +1,37 @@
 from django.http import HttpResponse
 import json
+from .models import Service
 
 
 def index(request):
-    with open('Baltback/apps/services/res/services.json') as services_json:
-        d = dict(json.load(services_json))
-        http_resp = ""
-        for service in d.get("services"):
-            http_resp += "<h1>%s</h1>\n" % service.get("name")
-            http_resp += "<h2>%s</h2>\n" % service.get("intro")
+    mode = 'db'
+    if mode == 'db':
+        http_resp = ''
+        for service in Service.objects.all():
+            http_resp += "<h1>%s</h1>\n" % service.title
+            http_resp += "<h2>%s</h2>\n" % service.intro
+
             http_resp += "<ul>\n"
-            for serv_desc in service.get("service_list"):
-                http_resp += "<li>%s</li>\n" % serv_desc
+            for point in json.loads(service.enum_json).get("enum"):
+                http_resp += "<li>%s</li>\n" % point
             http_resp += "</ul>\n"
-            http_resp += "<h2>%s</h2>\n\n\n" % service.get("outro")
+
+            http_resp += "<h2>%s</h2>\n\n\n" % service.outro
+
+
+    elif mode == 'json':
+        with open('Baltback/apps/services/res/services.json') as services_json:
+            d = dict(json.load(services_json))
+            http_resp = ""
+            for service in d.get("services"):
+                http_resp += "<h1>%s</h1>\n" % service.get("name")
+                http_resp += "<h2>%s</h2>\n" % service.get("intro")
+
+                http_resp += "<ul>\n"
+                for serv_desc in service.get("enum"):
+                    http_resp += "<li>%s</li>\n" % serv_desc
+                http_resp += "</ul>\n"
+
+                http_resp += "<h2>%s</h2>\n\n\n" % service.get("outro")
 
     return HttpResponse(http_resp)
